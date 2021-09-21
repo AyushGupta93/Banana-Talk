d65651c4b6c2122341183d6811abbd1c1c9025e9
Ayush Gupta <ayushgupta.9350@gmail.com>
2021-09-21 21:22:35 +0530

    done

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-// var userdata = prompt('What is your name?');
-// var welcomeUser= 'welcome ' + userdata;
-// alert(welcomeUser);
-
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickEventHandler() {
-    // console.log("clicked");
-    // console.log("input", textInput.value);
-    outputDiv.innerText = "ahsjsal" 
+// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server! try again after some time");
+}
+
+function clickEventHandler() {
+    var inputText = textInput.value; // taking input 
+
+    // calling server for processing
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickEventHandler);
